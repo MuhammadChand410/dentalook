8f09259746e868d9160f47b18b9f6f4ad8cccddd
MuhammadChand410 <muhammadchand00001@gmail.com>
2026-02-03 20:03:23 +0500

    add meta tagg

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,83 +1,3 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Layouts from "../layouts/layouts";
-// import Tickets from "../pages/tickets/tickets";
-// import Analytics from "../pages/analytics/analytics";
-// import UserAudit from "../pages/user-audit/user-audit";
-// import Leaderboard from "../pages/leaderboard/leaderboard";
-// import Setting from "../pages/setting/setting";
-
-// import { lazy, Suspense } from "react";
-// import Login from "../pages/login/login";
-// const Dashboard = lazy(() => import("../pages/dashboard/dashboard"));
-
-// export default function MyRoutes() {
-//     return (
-//         <>
-//             <Suspense fallback={null}>
-//                 <BrowserRouter>
-//                     <Routes>
-//                         <Route path="/login" element={<Login />} />
-//                         <Route path="/" element={<Layouts />}>
-//                             <Route path="/" element={<Tickets />} />
-//                             <Route path="/dashboard" element={<Dashboard />} />
-//                             <Route path="/analytics" element={<Analytics />} />
-//                             <Route path="/user-audit" element={<UserAudit />} />
-//                             <Route path="/leaderboard" element={<Leaderboard />} />
-//                             <Route path="/settings" element={<Setting />} />
-//                         </Route>
-//                     </Routes>
-//                 </BrowserRouter>
-//             </Suspense>
-//         </>
-//     )
-// }
-
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-// import Tickets from "../pages/tickets/tickets";
-// import Analytics from "../pages/analytics/analytics";
-// import UserAudit from "../pages/user-audit/user-audit";
-// import Leaderboard from "../pages/leaderboard/leaderboard";
-// import Setting from "../pages/setting/setting";
-
-// import { lazy, Suspense } from "react";
-
-// import ProtectedRoute from "../pages/login/protected";
-// import Login from "../pages/login/login";
-// import Layouts from "../layouts/layouts";
-
-// const Dashboard = lazy(() => import("../pages/dashboard/dashboard"));
-
-// export default function MyRoutes() {
-//     return (
-//         <Suspense fallback={null}>
-//             <BrowserRouter>
-//                 <Routes>
-//                     {/* Public route */}
-//                     <Route path="/login" element={<Login />} />
-
-//                     {/* Protected routes */}
-//                     <Route
-//                         path="/"
-//                         element={
-//                             <ProtectedRoute>
-//                                 <Layouts />
-//                             </ProtectedRoute>
-//                         }
-//                     >
-//                         <Route path="/" element={<Tickets />} />
-//                         <Route path="/dashboard" element={<Dashboard />} />
-//                         <Route path="/analytics" element={<Analytics />} />
-//                         <Route path="/user-audit" element={<UserAudit />} />
-//                         <Route path="/leaderboard" element={<Leaderboard />} />
-//                         <Route path="/settings" element={<Setting />} />
-//                     </Route>
-//                 </Routes>
-//             </BrowserRouter>
-//         </Suspense>
-//     );
-// }
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Layouts from "../layouts/layouts";
@@ -89,7 +9,6 @@ import Leaderbooard from "../pages/leaderboard";
 import Useraudit from "../pages/user-audit";
 import Setings from "../pages/setting";
 import Logiin from "../pages/login";
-// import Login from "../pages/login/login";
 
 const Login = lazy(() => import("../pages/login/login"));
 const Dashboard = lazy(() => import("../pages/dashboard/dashboard"));
